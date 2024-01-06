5bc4b00add6937a657d6a14be94728de79f4802b
Egecan Giray <girayegecan02@gmail.com>
2024-01-06 20:32:13 +0100

    added image display

--- catalog/models.py
+++ catalog/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 
 
 # Create your models here.
 class Game(models.Model):
     title = models.CharField(max_length=150)
     description = models.TextField()
-    
+    cover = models.ImageField(upload_to='images/', null=True, blank=True)
+
+
+# Signal to delete the image file when a model instance is deleted
+@receiver(pre_delete, sender=Game)
+def delete_image_file(sender, instance, **kwargs):
+    if instance.cover:
+        instance.cover.delete(save=False)
