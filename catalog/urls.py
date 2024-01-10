7fa79fdf17fb2ff4698bc976315292ea1e65114f
Egecan Giray <girayegecan02@gmail.com>
2024-01-10 16:23:05 +0100

    github?

--- catalog/urls.py
+++
@@ -1,8 +0,0 @@
-# catalog/urls.py
-from django.urls import path
-from .views import game_list, GameDetailView
-
-urlpatterns = [
-    path('games/', game_list, name='game-list'),
-    path('games/<int:pk>/', GameDetailView.as_view(), name='game_detail'),
-]
